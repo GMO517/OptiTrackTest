fd68f9362733e4148b602dbc7e3b646e56dda490
yanshou517 <yanshou.su@yunhetech.com.tw>
2023-10-27 10:56:43 +0800

    [更新] 攝影機位置調整 追蹤功能暫時關閉

--- Assets/OptiTrack/Scripts/SkeletonTransform.cs
+++ Assets/OptiTrack/Scripts/SkeletonTransform.cs
@@ -19,29 +19,29 @@ public class SkeletonTransform : MonoBehaviour
 
     void Start()
     {
-        animator = GetComponent<Animator>();
-        followCube = GameObject.Find("CubeHead");
-        followLHand = GameObject.Find("CubeLHand");
-        followRHand = GameObject.Find("CubeRHand");
+        //animator = GetComponent<Animator>();
+        //followCube = GameObject.Find("CubeHead");
+        //followLHand = GameObject.Find("CubeLHand");
+        //followRHand = GameObject.Find("CubeRHand");
 
     }
     void Update()
     {
-        head = animator.GetBoneTransform(HumanBodyBones.Head);
-        //hip = animator.GetBoneTransform(HumanBodyBones.Hips);
-        leftHand = animator.GetBoneTransform(HumanBodyBones.LeftHand);
-        rightHand = animator.GetBoneTransform(HumanBodyBones.RightHand);
-        //leftFoot = animator.GetBoneTransform(HumanBodyBones.LeftFoot);
-        //rightFoot = animator.GetBoneTransform(HumanBodyBones.RightFoot);
-        //Debug.Log("頭: " + head.position);
-        //Debug.Log("2: " + hip.position);
-        //Debug.Log("左手: " + leftHand.position);
-        //Debug.Log("右手: " + rightHand.position);
-        //Debug.Log("5: " + leftFoot.position);
-        //Debug.Log("6: " + rightFoot.position);
-        followCube.transform.SetPositionAndRotation(head.position, head.rotation);
-        followLHand.transform.SetPositionAndRotation(leftHand.position, leftHand.rotation);
-        followRHand.transform.SetPositionAndRotation(rightHand.position, rightHand.rotation);
+        //head = animator.GetBoneTransform(HumanBodyBones.Head);
+        ////hip = animator.GetBoneTransform(HumanBodyBones.Hips);
+        //leftHand = animator.GetBoneTransform(HumanBodyBones.LeftHand);
+        //rightHand = animator.GetBoneTransform(HumanBodyBones.RightHand);
+        ////leftFoot = animator.GetBoneTransform(HumanBodyBones.LeftFoot);
+        ////rightFoot = animator.GetBoneTransform(HumanBodyBones.RightFoot);
+        ////Debug.Log("頭: " + head.position);
+        ////Debug.Log("2: " + hip.position);
+        ////Debug.Log("左手: " + leftHand.position);
+        ////Debug.Log("右手: " + rightHand.position);
+        ////Debug.Log("5: " + leftFoot.position);
+        ////Debug.Log("6: " + rightFoot.position);
+        //followCube.transform.SetPositionAndRotation(head.position, head.rotation);
+        //followLHand.transform.SetPositionAndRotation(leftHand.position, leftHand.rotation);
+        //followRHand.transform.SetPositionAndRotation(rightHand.position, rightHand.rotation);
 
     }
 }

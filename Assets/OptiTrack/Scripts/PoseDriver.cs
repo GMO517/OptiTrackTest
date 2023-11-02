538986208f5a33ed717db30c84556ef559593c0c
yanshou517 <yanshou.su@yunhetech.com.tw>
2023-11-02 16:27:22 +0800

    [更新] 手部補正

--- Assets/OptiTrack/Scripts/PoseDriver.cs
+++ Assets/OptiTrack/Scripts/PoseDriver.cs
@@ -25,34 +25,7 @@ public class PoseDriver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (track.trackHead != null)
-        {
-            Pose HeadPose = GetPose(track.trackHead);
-            //Quaternion yOffset = new Quaternion(-HeadPose.rotation.y, -HeadPose.rotation.z, HeadPose.rotation.x, HeadPose.rotation.w);
-            Quaternion yOffset = Quaternion.Euler(-90f, 0f, 90f); //人物軸心跟攝影機不同的話要記得改這邊 看XYZ軸相差多少
-            cubeHead.transform.position = HeadPose.position;
-            cubeHead.transform.rotation = HeadPose.rotation * yOffset;
-
-            dice.transform.position = HeadPose.position;
-            dice.transform.rotation = HeadPose.rotation;
-
-            Debug.Log("頭數據1:" + HeadPose.position.GetType());
-            Debug.Log("頭位置:" + cubeHead.transform.position);
-            Debug.Log("頭數據2:" + HeadPose.rotation);
-            Debug.Log("頭轉:" + cubeHead.transform.rotation);
-        }
-        if (track.trackLeftHand != null)
-        {
-            Pose LhandPose = GetPose(track.trackLeftHand);
-            cubeLhand.transform.SetPositionAndRotation(LhandPose.position, LhandPose.rotation);
-            //Debug.Log("左手位置: " + cubeLhand.transform.position + " 旋轉: " + cubeLhand.transform.rotation);
-        }
-        if (track.trackRightHand != null)
-        {
-            Pose RhandPose = GetPose(track.trackRightHand);
-            cubeRhand.transform.SetPositionAndRotation(RhandPose.position, RhandPose.rotation);
-            //Debug.Log("右手位置: " + cubeRhand.transform.position + " 旋轉: " + cubeRhand.transform.rotation);
-        }
+        UpdatePose();
     }
 
     public Pose GetPose(Transform targetTrans)
@@ -61,25 +34,40 @@ public class PoseDriver : MonoBehaviour
         return pose;
     }
 
-    //轉換坐標系統用 動補是右手系 unity是左手系
-    private Vector3 ConvertCoordinateSystemPos(Vector3 rightHandedVector)
+    private void UpdatePose()
     {
-        return new Vector3(rightHandedVector.x, rightHandedVector.z, rightHandedVector.y);
-    }
+        if (track.trackHead != null)
+        {
+            Pose HeadPose = GetPose(track.trackHead);
+            Quaternion yOffset = Quaternion.Euler(-90f, 0f, 90f); //人物軸心跟攝影機不同的話要記得改這邊 看XYZ軸相差多少
+            cubeHead.transform.position = HeadPose.position;
+            cubeHead.transform.rotation = HeadPose.rotation * yOffset;
 
-    private Quaternion ConvertCoordinateSystemRot(Quaternion rightHandedQuaternion)
-    {
-        return new Quaternion(
-            -rightHandedQuaternion.x,
-            -rightHandedQuaternion.z,
-            -rightHandedQuaternion.y,
-            rightHandedQuaternion.w);
+            dice.transform.position = HeadPose.position;
+            dice.transform.rotation = HeadPose.rotation;
 
-        //return new Quaternion(
-        //    -rightHandedQuaternion.x,
-        //    -rightHandedQuaternion.y,
-        //    -rightHandedQuaternion.z,
-        //    rightHandedQuaternion.w);
-
+            //Debug.Log("頭數據1:" + HeadPose.position.GetType());
+            //Debug.Log("頭位置:" + cubeHead.transform.position);
+            //Debug.Log("頭數據2:" + HeadPose.rotation);
+            //Debug.Log("頭轉:" + cubeHead.transform.rotation);
+        }
+        if (track.trackLeftHand != null)
+        {
+            Pose LhandPose = GetPose(track.trackLeftHand);
+            Quaternion lHandOffset = Quaternion.Euler(-90f, 0f, 90f); //人物軸心跟攝影機不同的話要記得改這邊 看XYZ軸相差多少
+            cubeLhand.transform.SetPositionAndRotation(LhandPose.position, LhandPose.rotation * lHandOffset);
+            Vector3 leftAng = cubeLhand.transform.rotation.eulerAngles;
+            //Debug.Log("左手位置: " + cubeLhand.transform.position + " 旋轉: " + cubeLhand.transform.rotation);
+            Debug.Log("左手位置: " + cubeLhand.transform.position + " 旋轉: " + leftAng);
+        }
+        if (track.trackRightHand != null)
+        {
+            Pose RhandPose = GetPose(track.trackRightHand);
+            Quaternion rHandOffset = Quaternion.Euler(-90f, 0f, 90f); //人物軸心跟攝影機不同的話要記得改這邊 看XYZ軸相差多少
+            cubeRhand.transform.SetPositionAndRotation(RhandPose.position, RhandPose.rotation * rHandOffset);
+            //Debug.Log("右手位置: " + cubeRhand.transform.position + " 旋轉: " + cubeRhand.transform.rotation);
+            Vector3 rightAng = cubeRhand.transform.rotation.eulerAngles;
+            Debug.Log("右手位置: " + cubeRhand.transform.position + " 旋轉: " + rightAng);
+        }
     }
 }
--- Assets/OptiTrack/Scripts/SkeletonTrack.cs
+++ Assets/OptiTrack/Scripts/SkeletonTrack.cs
@@ -11,44 +11,10 @@ public class SkeletonTrack : MonoBehaviour
     public Transform trackLeftFoot;
     public Transform trackRightFoot;
 
-    //public Animator animator;
-
-    //public GameObject followHead;
-    //public GameObject followLHand;
-    //public GameObject followRHand;
-
-    //public Transform poseTrans;
-
-    void Start()
+    private OptitrackSkeletonAnimator skeletonScript;
+    private void Start()
     {
-        //animator = GetComponent<Animator>();
-        GetBoneTransform();
-    }
-    void Update()
-    {
-        //GetBoneTransform();
-        //Debug.Log("位置: " + trackHead.position);
-        //Debug.Log("位置2: " + trackHead.transform.position);
-
-        //Debug.Log("旋轉: " + trackHead.rotation);
-        //Debug.Log("旋轉: " + trackHead.transform.rotation);
-
-
-    }
-
-    private void GetBoneTransform()
-    {
-        //trackHead = animator.GetBoneTransform(HumanBodyBones.Head);
-        //trackHip = animator.GetBoneTransform(HumanBodyBones.Hips);
-        //trackLeftHand = animator.GetBoneTransform(HumanBodyBones.LeftHand);
-        //trackRightHand = animator.GetBoneTransform(HumanBodyBones.RightHand);
-        //trackLeftFoot = animator.GetBoneTransform(HumanBodyBones.LeftFoot);
-        //trackRightFoot = animator.GetBoneTransform(HumanBodyBones.RightFoot);
-    }
-
-    public void GetPose()
-    {
-        //poseTrans = trackHead.transform;
+        skeletonScript = this.GetComponent<OptitrackSkeletonAnimator>();
     }
 
 
